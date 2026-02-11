39622028011313131d839a63028f98a930d7d863
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-02-11 11:29:22 +0530

    added card portion there

--- Frontend_CarRental/src/pages/Cardetails.jsx
+++ Frontend_CarRental/src/pages/Cardetails.jsx
@@ -15,7 +15,7 @@ const Cardetails = () => {
   return car ? (
     <div className='px-6 mt-15'>
       <button
-        className='flex items-center gap-2 mb-6'
+        className='flex items-center gap-2 mb-6 ml-10'
         onClick={() => navigate(-1)} >
         <img src={assets.arrow_icon} className='rotate-180' alt="" />
         Back to all Car
@@ -23,12 +23,12 @@ const Cardetails = () => {
 
 
 
-      <div className='grid grid-cols-3 gap-12'>
-        {/* {left} */}
-        <div className='col-span-2 mb-25'>
+      <div className='grid grid-cols-3 gap-12 m-auto ml-10 mr-10'>
+  
+        <div className='col-span-2 mb-25 '>
           <img src={car.image} alt="" className='w-full object-cover mb-6 h-130 rounded-2xl' />
           <div>    <h1 className='text-4xl font-semibold mb-3'>{car.brand}   {car.model}</h1>
-            <p className='text-lg  font-serif'>{car.year }  { car.category}</p>
+            <p className='text-lg  font-serif'>{car.year}  {car.category}</p>
           </div>
           <hr />
           <div className="grid grid-cols-4 gap-4 mt-6">
@@ -55,23 +55,52 @@ const Cardetails = () => {
           <div className='mt-1'>
             <h1 className='font-semibold text-2xl mb-2'>Features</h1>
             <div className='grid grid-cols-2'>
-             {[
-              '360 Camera','GPS','heated Seats','Rear view'
-             ].map((item)=>(
-              <li className='flex items-center text-gray-600 gap-3  ' key={item}>
-                <img src={assets.check_icon} alt="" />
-                {item}
-              </li>
-             ))}
+              {[
+                '360 Camera', 'GPS', 'heated Seats', 'Rear view'
+              ].map((item) => (
+                <li className='flex items-center text-gray-600 gap-3  ' key={item}>
+                  <img src={assets.check_icon} alt="" />
+                  {item}
+                </li>
+              ))}
             </div>
           </div>
         </div>
 
 
-        {/* {right} */}
+      
         <div>
-          <form action="">
-            ddd
+          <form action="" className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+            <div className='flex justify-between items-center'>
+              <p>$ {car.pricePerDay}</p>
+              <span> Per Day</span>
+            </div>
+            <hr className="border-borderColor my-6" />
+            <div className="flex flex-col items-start">
+              <label htmlFor="pickup-date">PickUp Date</label>
+              <input
+                type="date"
+                id='pickup-date'
+                min={new Date().toISOString().split('T')[0]}
+                className="border  border-borderColor px-3 py-2 w-99 rounded-lg text-gray-600  mt-3 outline-none cursor-pointer"
+                required
+
+              />
+            </div>
+            <div className="flex flex-col items-start">
+              <label htmlFor="return-date">Return Date</label>
+              <input
+                type="date"
+                id='return-date'
+                min={new Date().toISOString().split('T')[0]}
+                className="border border-borderColor px-5 w-99 py-3 cursor-pointer rounded-lg"
+                required
+
+              />
+            </div>
+            <button className='w-full bg-blue-500 hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>Book Now</button>
+
+
           </form>
         </div>
       </div>
