02e0cfd9300d047c08b6d773706ccb5ade557991
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-02-14 22:28:43 +0530

    added sidebar.jsx

---
+++ Frontend_CarRental/src/components/owner/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { dummyUserData, ownerMenuLinks } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const user = dummyUserData
+
+  return (
+    <div className="relative top-0 left-0 w-72 h-screen border-r bg-white shadow-sm">
+      <div className="space-y-1">
+        {ownerMenuLinks.map((link, index) => (
+          <NavLink
+            key={index}
+            to={link.path}
+            end={link.path === "/owner"}
+            className={({ isActive }) =>
+              `flex items-center gap-4 w-full py-6 font-normal pl-5 transition-all duration-200
+               hover:bg-sky-200
+               ${isActive
+                ? "bg-sky-200 text-blue-600 font-medium border-r-4 border-blue-500"
+                : "text-gray-600"}`
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <img
+                  src={isActive ? link.coloredIcon : link.icon}
+                  alt={link.name}
+                  className="w-5 h-5"
+                />
+                {link.name}
+              </>
+            )}
+          </NavLink>
+        ))}
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
