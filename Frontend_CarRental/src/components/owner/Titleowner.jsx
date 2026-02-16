c0020599b92b94000ea39bb5ae63dbef7f383e6b
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-02-16 17:20:51 +0530

    added app context

--- Frontend_CarRental/src/components/owner/Titleowner.jsx
+++ Frontend_CarRental/src/components/owner/Titleowner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Titleowner = ({title,subtitle}) => {
   return (
     <div>
-        <h1 className='font-semibold  text-2xl'>{title}</h1>
+        <h1 className='font-semibold font-sans  text-3xl'>{title}</h1>
         <p className='font-normal mt-2 text-gray-500 '>{subtitle} </p>
     </div>
   )
---
+++ Frontend_CarRental/src/context/AppContext.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000/api/v1"
+})
+const AppContext = ({ children }) => {
+
+    const [token, setTokens] = useState(localStorage.getItem('token'))
+    const [user, setUser] = useState(null)
+    const [car, setCar] = useState(null)
+    const [isOwner, setIsOwner] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const getUser = async () => {
+        try {
+            const { data } = await api.get("/user/getuser");
+            setUser(data)
+            if (data.role === "owner") {
+                setIsOwner(true);
+            } else {
+                setIsOwner(false)
+            }
+
+        } catch (error) {
+            logout();
+        }
+    }
+
+    return (
+        <AppContext.Provider value={{ token, user }}>{children}</AppContext.Provider>
+    )
+}
+
+export default AppContext
