d6be74ba6f7c70d9f46aa372157dd34a18696c37
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-02-21 22:40:29 +0530

    added dashboard styling there

--- Frontend_CarRental/src/pages/owner/Dashboard.jsx
+++ Frontend_CarRental/src/pages/owner/Dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Titleowner from '../../components/owner/Titleowner'
 import { assets, dummyDashboardData } from '../../assets/assets'
 
 const Dashboard = () => {
   const data = dummyDashboardData
+  const [Data, setData] = useState({
+    totalCars: 0,
+    totalBookings: 0,
+    pendingBookings: 0,
+    completeBookings: 0,
+    recentBookings: [],
+    monthlyRevenue: 0
+  })
+  useEffect(() => {
+    setData(dummyDashboardData)
+  }, []
+  )
 
   return (
     <div className="p-8">
@@ -12,7 +24,6 @@ const Dashboard = () => {
         title="Admin Dashboard"
         subtitle="Monitor overall platform performance including total cars, bookings, revenue, and recent activities"
       />
-
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8">
 
         <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
@@ -26,7 +37,7 @@ const Dashboard = () => {
             className="w-10 h-9 object-contain"
           />
         </div>
-      <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
+        <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
           <div>
             <p className="text-gray-600">Total Bookings</p>
             <p className="text-2xl text-center font-bold">{data.totalBookings}</p>
@@ -37,7 +48,7 @@ const Dashboard = () => {
             className="w-10 h-9 object-contain"
           />
         </div>
-              <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
+        <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
           <div>
             <p className="text-gray-600">Pending Booking</p>
             <p className="text-2xl text-center font-bold">{data.pendingBookings}</p>
@@ -48,7 +59,7 @@ const Dashboard = () => {
             className="w-10 h-9 object-contain"
           />
         </div>
-              <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
+        <div className="bg-white shadow-lg rounded-xl py-7 px-10 gap-5 flex items-center justify-center">
           <div>
             <p className="text-gray-600">Complete Booking</p>
             <p className="text-2xl text-center font-bold">{data.completedBookings}</p>
@@ -60,10 +71,41 @@ const Dashboard = () => {
           />
         </div>
       </div>
-      <div>
-      <h1>  Recent Booking</h1>
-      <p></p>
+
+      <div className='flex flex-wrap items-start gap-12 mb-8 w-full mt-12'>
+        <div className='p-6 border-2 border-amber-950 rounded-md'>
+          <h1 className='text-xl font-medium'>Recent Booking</h1>
+          <h1 className='text-gray-600 font-normal'>Latest Customer Bookings </h1>
+          {
+            data.recentBookings.map((booking, index) => (
+              <div key={index} className='mt-4 flex items-center gap-8 justify-between'>
+                <div className='flex items-center gap-2'>
+                  <div className='flex items-center justify-center w-13 h-13 rounded-full'>
+                    <img src={assets.listIconColored} alt="" />
+                  </div>
+                  <div>
+                    <p>{booking.car.brand} {booking.car.model}</p>
+                    <p className='text-sm text-gray-600'>{booking.createdAt.split('T')[0]}</p>
+                  </div>
+                </div>
+                <div className='flex items-center gap-4 font-medium '>
+                  <p className='text-lg text-gray-900'>$ {booking.price}</p>
+                  <p className='px-5 py-1 border-2 border-amber-950 rounded-full'>{booking.status}</p>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+        {/* revenue */}
+        <div className='p-6 mb-6 border-2 border-amber-950 rounded-md '>
+          <h1 className='text-lg  font-medium' >Monthly Revenue</h1>
+          <p className='text-gray-500'>Revenue for current month</p>
+          <p className='text-3xl mt-6 font-semibold '>$ {data.monthlyRevenue} </p>
+        </div>
+       
       </div>
+
     </div>
   )
 }
