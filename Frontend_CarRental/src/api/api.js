c9e04d85e42a9c6fabfb827d1b6f0552a350b32a
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-02-28 18:17:41 +0530

    added fix in api.js

--- Frontend_CarRental/src/api/api.js
+++ Frontend_CarRental/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1"
+  baseURL: "http://localhost:3000/api/v1",
+    withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
